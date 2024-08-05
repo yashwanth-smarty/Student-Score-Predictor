c157d1ba4c57c1585cf5f9af29c10bd9deebc6ed
Yashwanth <kumaryashwanth198@gmail.com>
2024-08-05 10:00:31 +0530

    Hyperparametering is done

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -17,12 +17,18 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
             predict=model.predict(X_test)
             score=r2_score(y_test,predict)
